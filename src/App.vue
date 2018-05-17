cfe9f70dcc89110e83be9ebc9186b4113bab23f2
Alexey Shevchenko <kamikozz@mail.ru>
2018-05-17 07:14:50 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,12 @@
-<template>
-  <div id="app">
-	<v-app>
-		<v-content>
+<template><div id="app"><v-app><v-content>
 			<v-container class="mt-5" grid-list-md text-xs-center>
         <v-layout row wrap>
           <v-flex xs10 offset-xs1>
 			 <v-form v-on:submit.prevent="addNote">
-				<v-text-field
-					v-model="input"
-					solo
-					placeholder=""
-					no-resize
-					hide-details
-					color="primary"
-
-				></v-text-field>
-				<v-btn
-					@click="addNote"
-					v-bind:disabled="!hasInputText"
-					outline
-
-					color="primary"
-				>
-				add
-				</v-btn>
+				<v-text-field v-model="input" solo placeholder="" no-resize hide-details color="primary"></v-text-field>
+				<v-btn @click="addNote" v-bind:disabled="!hasInputText" outline color="primary">add</v-btn>
        </v-form>
-        <v-data-table
-          :items="notes"
-          hide-actions
-          hide-headers
-          class="elevation-1"
-        >
+        <v-data-table :items="notes" hide-actions hide-headers class="elevation-1">
           <template slot="items" slot-scope="props">
             <td>
               <v-layout row wrap>
@@ -52,7 +28,4 @@
           </v-flex>
         </v-layout>
 			</v-container>
-		</v-content>
-	</v-app>
-  </div>
-</template>
+		</v-content></v-app></div></template>
